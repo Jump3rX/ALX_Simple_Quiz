7c7b09f165d8df33ca7633d7260437e7aa9be426
Conrad <33659768+Jump3rX@users.noreply.github.com>
2025-01-19 15:04:48 +0300

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -4,7 +4,7 @@ function checkAnswer() {
   let correctAnswer = "4";
   let checkedRadio = document.querySelector("input[name='quiz']:checked");
   let userAnswer = checkedRadio.value;
-  if (userAnswer == correctAnswer) {
+  if (userAnswer === correctAnswer) {
     feedbackInput.textContent = "Correct! Well done.";
   } else {
     feedbackInput.textContent = "That's incorrect. Try again!";
