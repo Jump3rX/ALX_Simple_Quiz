f332184658411196dec35fc5eb4e893cf4da6788
conrad <conradmax5@gmail.com>
2025-01-19 15:36:35 +0300

    fix return

--- calculator.js
+++ calculator.js
@@ -6,41 +6,41 @@ var divideBtn = document.getElementById("divide");
 var resultEl = document.getElementById("calculation-result");
 
 function add(num1, num2) {
-  resultEl.textContent = num1 + num2;
+  return num1 + num2;
 }
 
 function subtract(num1, num2) {
-  resultEl.textContent = num1 - num2;
+  return num1 - num2;
 }
 
 function multiply(num1, num2) {
-  resultEl.textContent = num1 * num2;
+  return num1 * num2;
 }
 
 function divide(num1, num2) {
-  resultEl.textContent = num1 / num2;
+  return num1 / num2;
 }
 
 addBtn.addEventListener("click", function () {
   let numberOne = parseFloat(document.getElementById("number1").value) || 0;
   let numberTwo = parseFloat(document.getElementById("number2").value) || 0;
-  add(numberOne, numberTwo);
+  resultEl.textContent = add(numberOne, numberTwo);
 });
 
 subtractBtn.addEventListener("click", function () {
   let numberOne = parseFloat(document.getElementById("number1").value) || 0;
   let numberTwo = parseFloat(document.getElementById("number2").value) || 0;
-  subtract(numberOne, numberTwo);
+  resultEl.textContent = subtract(numberOne, numberTwo);
 });
 
 multiplyBtn.addEventListener("click", function () {
   let numberOne = parseFloat(document.getElementById("number1").value) || 0;
   let numberTwo = parseFloat(document.getElementById("number2").value) || 0;
-  multiply(numberOne, numberTwo);
+  resultEl.textContent = multiply(numberOne, numberTwo);
 });
 
 divideBtn.addEventListener("click", function () {
   let numberOne = parseFloat(document.getElementById("number1").value) || 0;
   let numberTwo = parseFloat(document.getElementById("number2").value) || 0;
-  divide(numberOne, numberTwo);
+  resultEl.textContent = divide(numberOne, numberTwo);
 });
